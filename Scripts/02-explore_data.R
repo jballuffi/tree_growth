362b9afadec403948e9e77dc4e572fdb055e8af7
Juliana Balluffi-Fry <38500356+jballuffi@users.noreply.github.com>
2024-06-11 14:23:49 -0600

    more models

--- Scripts/02-explore_data.R
+++ Scripts/02-explore_data.R
@@ -15,29 +15,24 @@ dat <- dat[year > 1985]
 
 str(dat)
 
+onetree <- dat[tree == "KL-G5"]
+twotree <- dat[tree == "KL-G5" | tree == "KL-21"]
 
 
+mastcols <- c("no" = "grey20", "yes" = "red")
 
-ggplot(dat)+
-  geom_point(aes(x = year, y = PAAI, color = mast))
+ggplot(twotree)+
+  geom_point(aes(x = year, y = PAAI, color = mast), size = 2)+
+  geom_path(aes(x = year, y = PAAI, group = tree, linetype = tree), size = .5)+
+  scale_color_manual(values = mastcols)+
+  labs(y = "PAAI (% annual increment)", x = "Year")+
+  theme_minimal()
 
-ggplot(dat)+
-  geom_point(aes(x = age, y = PAAI))
-
-ggplot(dat)+
-  geom_point(aes(x = cmi, y = PAAI))
-
-ggplot(dat)+
-  geom_point(aes(x = cones, y = PAAI))
-
-ggplot(dat)+
-  geom_boxplot(aes(x = mast, y = PAAI))
-
-ggplot(dat)+
-  geom_boxplot(aes(x = summer_mean_temp, y = PAAI))
 
 
 
+ggplot(dat)+
+  geom_point(aes(x = year, y = PAAI))
 
 
 # AIC model comparisons --------------------------------------------------------------------
@@ -67,3 +62,46 @@ R2s$Modnames <- Names
 
 AIC <- merge(AIC, R2s, by = "Modnames")
 
+
+
+
+# change in PAAI ----------------------------------------------------------
+
+ggplot(dat)+
+  geom_point(aes(x = age, y = PAAI))+
+  geom_smooth(aes(x = age, y = PAAI), method = "lm")
+
+ggplot(dat)+
+  geom_boxplot(aes(x = mast, y = PAAI_diff))
+
+
+#I am including age in all models because it has a significant effect on PAAI
+#something we want to control for. question is do we use age as a random effect?
+null <- lmer(PAAI_diff ~ age + (1|tree), dat) 
+mast <- lmer(PAAI_diff ~ mast + age + (1|tree), dat)
+cones <- lmer(PAAI_diff ~ cones + age + (1|tree), dat)
+cmi <- lmer(PAAI_diff ~ cmi + age + (1|tree), dat)
+cmimast <- lmer(PAAI_diff ~ cmi + mast + age + (1|tree), dat)
+cmicones <- lmer(PAAI_diff ~ cmi + cones + age + (1|tree), dat)
+
+#FROM OTHER SCRIPT NOT CHANGED YET
+Mods <- list(null, mast, cones, cmi, cmimast, cmicones)
+Names <- c('Null', 'Mast', 'Cones', 'CMI', 'CMI_Mast', 'CMI_cones')
+AIC <- as.data.table(aictab(REML = F, cand.set = Mods, modnames = Names, sort = TRUE))
+AIC[, ModelLik := NULL]
+AIC[, Cum.Wt := NULL]
+#round whole table to 3 dec places
+AIC <- AIC %>% mutate_if(is.numeric, round, digits = 3)
+
+#run collectR2 function and get R2s for all models
+R2s <- lapply(Mods, collectR2)
+R2s <- rbindlist(R2s, fill = TRUE)
+R2s$Modnames <- Names
+
+AIC <- merge(AIC, R2s, by = "Modnames")
+
+
+
+ggplot(dat)+
+  geom_point(aes(x = age, y = PAAI_diff, color = mast))+
+  scale_color_manual(values = mastcols)
